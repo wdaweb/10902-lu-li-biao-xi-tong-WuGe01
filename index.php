460295b4ff88b924950cf6ab3cfbcf63e3add8ae
吳宗融 <bc781208@hotmail.com>
2020-07-01 16:15:46 +0800

    版型初步完成

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <link rel="stylesheet" href="./plugins//bootstrap.css">
+    <link rel="stylesheet" href="mystyle.css">
+    <title>我的電子履歷表</title>
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="container">
+                <a href="?do=main"><img class="headerIn" src="./img/logo-01.png"></a> 
+                <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=log">後台</a>
+                <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=tel">聯繫</a>
+                <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=art">作品</a>
+                <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=exp">經歷</a>
+                <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=skill">技能</a>
+                <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=res">自傳</a>
+            </div>
+        </div>
+    </header>
+    <div class="back"></div>
+    <div class="big_dady">
+        <div class="main container">
+<?php
+    (!empty($_GET['do']))?include_once "./api/".$_GET['do'].".php":include_once "./api/main.php";
+?>  
+        </div>
+    </div>
+</body>
+</html>
