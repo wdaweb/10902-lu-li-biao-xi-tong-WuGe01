d948b2f0663ef64016791a21039de34e33ceaadc
吳宗融 <bc781208@hotmail.com>
2020-07-02 22:18:31 +0800

    完成初版6

--- backcontrol.php
+++ backcontrol.php
@@ -14,13 +14,14 @@
     <header>
         <div class="header">
             <div class="container">
-                <a href="?do=main"><img class="headerIn" src="./img/logo-01.png"></a> 
+                <a href="?do=main"><img class="headerIn" src="./img/logo-01.png"> </a> 
                 <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=log">登出</a>
                 <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=tel">聯繫</a>
                 <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=art">作品</a>
                 <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=exp">經歷</a>
                 <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=skill">技能</a>
                 <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=res">自傳</a>
+                <a class="headerWord btn btn-outline-secondary" style="border: 0px;" href="?do=main">首頁</a>
             </div>
         </div>
     </header>
@@ -28,7 +29,7 @@
     <div class="big_dady">
         <div class="main container">
             <div style="width: 100%;padding: 10px;">
-\
+
             </div>
         </div>
     </div>
